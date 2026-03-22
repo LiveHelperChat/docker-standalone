6cdfc4e92aa53c2106e00ed365098ad0c1ef0880
Remigijus Kiminas <remdex@gmail.com>
2026-03-22 02:15:34 -0400

    Control queue name from env file

--- conf/scripts/phpresque.sh
+++ conf/scripts/phpresque.sh
@@ -7,11 +7,20 @@ if [ -f /scripts/.env ]; then
 	workers_count=$(sed -n 's/^LHC_RESQUE_WORKERS_COUNT=//p' /scripts/.env | head -n 1)
 fi
 
+queue_name=''
+if [ -f /scripts/.env ]; then
+	queue_name=$(sed -n 's/^LHC_RESQUE_QUEUE=//p' /scripts/.env | head -n 1)
+fi
+
 case "$workers_count" in
 	''|*[!0-9]*) workers_count=2 ;;
 esac
 
+if [ -z "$queue_name" ]; then
+	queue_name='*'
+fi
+
 echo "Running live site cronjobs"
 ulimit -n 65535
 cd /code
-REDIS_BACKEND=redis:6379 REDIS_BACKEND_DB=1 INTERVAL=1 COUNT="$workers_count" VERBOSE=0 QUEUE='*' /usr/local/bin/php resque.php 2>&1
+REDIS_BACKEND=redis:6379 REDIS_BACKEND_DB=1 INTERVAL=1 COUNT="$workers_count" VERBOSE=0 QUEUE="$queue_name" /usr/local/bin/php resque.php 2>&1
